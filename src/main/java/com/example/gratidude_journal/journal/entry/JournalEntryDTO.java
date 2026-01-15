b87f23b4a3576176bd7ab2245bec838a9ae3b75d
Afeef Neiroukh <marblecake@posteo.com>
2026-01-15 14:25:04 +0100

    Add static method in JournalEntry to compare  between JournalEntryDTO- and JournalEntry objects

--- src/main/java/com/example/gratidude_journal/journal/entry/JournalEntryDTO.java
+++ src/main/java/com/example/gratidude_journal/journal/entry/JournalEntryDTO.java
@@ -1,8 +1,22 @@
 package com.example.gratidude_journal.journal.entry;
 
 public record JournalEntryDTO(JournalEntry.WellBeing wellBeing,
-        String gratefullForToday,
-        String gratefullForTodayDescription,
-        String gratefullForInLife,
-        String gratefullForInLifeDescription) {
+                String gratefullForToday,
+                String gratefullForTodayDescription,
+                String gratefullForInLife,
+                String gratefullForInLifeDescription) {
+
+        public static boolean compareToEntry(JournalEntryDTO entryDTO, JournalEntry entry) {
+                if (entryDTO == null && entry == null)
+                        return true;
+                if (entryDTO == null || entry == null)
+                        return false;
+                return entryDTO.wellBeing.equals(entry.getWellBeing())
+                                && entryDTO.gratefullForToday().equals(entry.getGratefullForToday())
+                                && entryDTO.gratefullForTodayDescription()
+                                                .equals(entry.getGratefullForTodayDescription())
+                                && entryDTO.gratefullForInLife().equals(entry.getGratefullForInLife())
+                                && entryDTO.gratefullForInLifeDescription()
+                                                .equals(entry.getGratefullForInLifeDescription());
+        }
 }
