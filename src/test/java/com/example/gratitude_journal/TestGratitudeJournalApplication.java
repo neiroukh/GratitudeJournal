593798c332ec6834236b6ccea298ad0a7bb09dc0
Afeef Neiroukh <marblecake@posteo.com>
2026-01-18 19:26:10 +0100

    Remove unecessary import

--- src/test/java/com/example/gratitude_journal/TestGratitudeJournalApplication.java
+++ src/test/java/com/example/gratitude_journal/TestGratitudeJournalApplication.java
@@ -2,12 +2,9 @@ package com.example.gratitude_journal;
 
 import org.springframework.boot.SpringApplication;
 
-import com.example.gratitude_journal.GratitudeJournalApplication;
-
 public class TestGratitudeJournalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.from(GratitudeJournalApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
-
 }
