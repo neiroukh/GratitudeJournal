1909c9feb3b374a4e61f52a5bcbc6d00a3a36869
Afeef Neiroukh <marblecake@posteo.com>
2026-01-25 12:25:03 +0100

    docs: add javadocs to JournalEntryModelAssembler

--- src/main/java/com/example/gratitude_journal/journal/entry/JournalEntryModelAssembler.java
+++ src/main/java/com/example/gratitude_journal/journal/entry/JournalEntryModelAssembler.java
@@ -8,17 +8,43 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
+/**
+ * Spring Component to create an EntityModel for a
+ * {@link com.example.gratitude_journal.journal.entry.JournalEntry} object. Used
+ * to add HAL-Support to JournalEntry responses.
+ * 
+ * @author Afeef Neiroukh
+ */
 @Component
 public class JournalEntryModelAssembler
-        implements RepresentationModelAssembler<JournalEntry, EntityModel<JournalEntry>> {
+                implements RepresentationModelAssembler<JournalEntry, EntityModel<JournalEntry>> {
 
-    @Override
-    public EntityModel<JournalEntry> toModel(JournalEntry journalEntry) {
-        return EntityModel.of(journalEntry,
-                linkTo(methodOn(JournalController.class).getEntry(journalEntry.getJournalEntryId())).withSelfRel(),
-                linkTo(methodOn(JournalController.class).deleteEntry(journalEntry.getJournalEntryId()))
-                        .withRel("delete"),
-                linkTo(methodOn(JournalController.class).updateEntry(journalEntry.getJournalEntryId(), null))
-                        .withRel("update"));
-    }
+        /**
+         * Returns the {@link com.example.gratitude_journal.journal.entry.JournalEntry}
+         * object and the following links in the "_links"
+         * property:
+         * <ul>
+         * <li>"self", points to the GET-Request of the journal entry</li>
+         * <li>"update", points to the PUT-Request of the journal entry</li>
+         * <li>"delete", points to the DELETE-Request of the journal entry</li>
+         * </ul>
+         * 
+         * @param journalEntry The
+         *                     {@link com.example.gratitude_journal.journal.entry.JournalEntry}
+         *                     object to wrap in an EntityModel object and add links to.
+         * @return An {@code EntityModel<JournalEntry>} object containing the
+         *         {@link com.example.gratitude_journal.journal.entry.JournalEntry}
+         *         object with links to possible operations on it.
+         */
+        @Override
+        public EntityModel<JournalEntry> toModel(JournalEntry journalEntry) {
+                return EntityModel.of(journalEntry,
+                                linkTo(methodOn(JournalController.class).getEntry(journalEntry.getJournalEntryId()))
+                                                .withSelfRel(),
+                                linkTo(methodOn(JournalController.class).updateEntry(journalEntry.getJournalEntryId(),
+                                                null))
+                                                .withRel("update"),
+                                linkTo(methodOn(JournalController.class).deleteEntry(journalEntry.getJournalEntryId()))
+                                                .withRel("delete"));
+        }
 }
